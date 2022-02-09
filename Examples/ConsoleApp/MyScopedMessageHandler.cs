7d56bdc85835bfd02d04bc6359ca510ebfbdfe86
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-02-09 15:09:47 +0330

    examples

--- Examples/ConsoleApp/MyScopedMessageHandler.cs
+++ Examples/ConsoleApp/MyScopedMessageHandler.cs
@@ -1,40 +1,26 @@
-﻿using Telegram.Bot.Types.ReplyMarkups;
-using Telegram.Bot.Types;
+﻿using Telegram.Bot.Types;
+using TelegramUpdater;
 using TelegramUpdater.UpdateContainer;
 using TelegramUpdater.UpdateHandlers.ScopedHandlers.ReadyToUse;
-using TelegramUpdater;
 
-namespace ConsoleApp
+namespace ConsoleApp;
+
+[ApplyFilter(typeof(PrivateTestCommand))]
+internal class MyScopedMessageHandler : ScopedMessageHandler
 {
-    [ApplyFilter(typeof(PrivateTestCommand))]
-    internal class MyScopedMessageHandler : ScopedMessageHandler
+    public MyScopedMessageHandler() : base(group: 0)
+    { }
+
+    protected override async Task HandleAsync(UpdateContainerAbs<Message> container)
     {
-        public MyScopedMessageHandler() : base(group: -1)
-        { }
-
-        protected override async Task HandleAsync(UpdateContainerAbs<Message> container)
-        {
-            var msg = await container.Response($"Are you ok? answer quick!",
-                replyMarkup: new InlineKeyboardMarkup(
-                    InlineKeyboardButton.WithCallbackData("Yes i'm OK!", "ok")));
-
-            await container.ChannelUserClick(TimeSpan.FromSeconds(5), "ok")
-                .IfNotNull(async answer =>
-                {
-                    await answer.Edit(text: "Well ...");
-                })
-                .Else(async _ =>
-                {
-                    await msg.Edit("Slow");
-                });
-        }
+        await container.Response("Tested!");
     }
+}
 
-    internal class PrivateTestCommand : Filter<Message>
+internal class PrivateTestCommand : Filter<Message>
+{
+    public PrivateTestCommand()
+        : base(FilterCutify.OnCommand("test") & FilterCutify.PM())
     {
-        public PrivateTestCommand()
-            : base(FilterCutify.OnCommand("test") & FilterCutify.PM())
-        {
-        }
     }
 }
