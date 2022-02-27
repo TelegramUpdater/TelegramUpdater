86b11eabb85700e151dd039e936a0d6748e846e4
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-02-27 22:28:05 +0330

    Another example

---
+++ Examples/QuickestPossible/Program.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using TelegramUpdater;
+
+var updater = new Updater(
+    new TelegramBotClient("BotToken"),
+    new UpdaterOptions(allowedUpdates: new[] { UpdateType.Message, UpdateType.CallbackQuery }))
+
+    .AddDefaultExceptionHandler()
+    .AutoCollectScopedHandlers();
+
+await updater.StartAsync();
