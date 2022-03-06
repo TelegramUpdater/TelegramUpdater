1164c37adb1755770fd8364760654ef074341ae7
MohammadReza <47322990+immmdreza@users.noreply.github.com>
2022-03-06 11:54:41 +0330

    Propagation exceptions are better be internal

--- TelegramUpdater/ExtensionMethods/PropagationExtensions.cs
+++ TelegramUpdater/ExtensionMethods/PropagationExtensions.cs
@@ -8,12 +8,12 @@ public static class PropagationExtensions
     /// <summary>
     /// All pending handlers for this update will be ignored after throwing this.
     /// </summary>
-    public static void StopPropagation<T>(this IContainer<T> _)
+    internal static void StopPropagation<T>(this IContainer<T> _)
         where T : class => throw new StopPropagation();
 
     /// <summary>
     /// Continue to the next pending handler for this update and ignore the rest of this handler.
     /// </summary>
-    public static void ContinuePropagation<T>(this IContainer<T> _)
+    internal static void ContinuePropagation<T>(this IContainer<T> _)
         where T : class => throw new ContinuePropagation();
 }
